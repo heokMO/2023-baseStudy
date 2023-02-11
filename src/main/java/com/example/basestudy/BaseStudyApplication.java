90c010079b9ce56977a37dbf3422c8a0187147b1
heokMO <gurah1220@gmail.com>
2023-02-11 21:29:06 +0900

    init

---
+++ src/main/java/com/example/basestudy/BaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.basestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basestudy/BaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
