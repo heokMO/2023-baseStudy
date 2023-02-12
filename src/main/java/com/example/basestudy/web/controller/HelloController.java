bc965b5f561805ad80047c504d47dac8b2a34edd
heokMO <gurah1220@gmail.com>
2023-02-12 22:17:03 +0900

    Controller HelloResponseDto 반환 추가

--- src/main/java/com/example/basestudy/web/controller/HelloController.java
+++ src/main/java/com/example/basestudy/web/controller/HelloController.java
@@ -1,13 +1,24 @@
 package com.example.basestudy.web.controller;
 
+import com.example.basestudy.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping
     public String hello(){
         return "Hello";
+    }
+
+    @GetMapping("/dto")
+    public HelloResponseDto helloDto(@RequestParam String name,
+                                     @RequestParam int amount){
+
+        return new HelloResponseDto(name, amount);
     }
 }
