f1d67ff0a5876deeda85805c1589dcf9fe7b517b
heokMO <gurah1220@gmail.com>
2023-02-15 04:12:07 +0900

    Create ExceptionHandler for PostApiController

--- src/main/java/com/example/basestudy/web/controller/PostsApiController.java
+++ src/main/java/com/example/basestudy/web/controller/PostsApiController.java
@@ -5,6 +5,8 @@ import com.example.basestudy.web.dto.PostUpdateRequestDto;
 import com.example.basestudy.web.dto.PostsResponseDto;
 import com.example.basestudy.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -15,17 +17,25 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long  save(@RequestBody PostsSaveRequestDto requestDto){
+
         return postsService.save(requestDto);
     }
 
     @PutMapping("/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostUpdateRequestDto requestDto){
+
         return postsService.update(id, requestDto);
     }
     @GetMapping("/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
+
         return postsService.findById(id);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
 }
